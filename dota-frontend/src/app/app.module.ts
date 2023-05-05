f273d889fb42b9e8dc7eefb74648967907c853e4
unknown <hussain.qurain@outlook.com>
2023-05-05 20:36:33 +0300

    deleted nebular and installed angular material

--- dota-frontend/src/app/app.module.ts
+++ dota-frontend/src/app/app.module.ts
@@ -4,8 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
-import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
@@ -17,9 +15,6 @@ import { HeaderComponent } from './components/header/header.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NbThemeModule.forRoot({ name: 'dark' }),
-    NbLayoutModule,
-    NbEvaIconsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
