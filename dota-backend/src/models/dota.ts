2b8bbae45cd03cd4d9faf57ecb3b617fb3f962b4
unknown <hussain.qurain@outlook.com>
2023-05-15 22:38:04 +0300

    added comment for dota model method

--- dota-backend/src/models/dota.ts
+++ dota-backend/src/models/dota.ts
@@ -32,6 +32,7 @@ export default class Dota {
             //get all live matches
             const matches: LiveMatch[] = await this.get_live_matches();
             //find the match that contains the player with the given ID
+            //use find method to return the first match that contains the player with the given ID
             const match = matches.find(match => match.players.find(player => player.account_id === account_id && match.deactivate_time === 0));
             //if match is found, return all players in that match
             if(match){
