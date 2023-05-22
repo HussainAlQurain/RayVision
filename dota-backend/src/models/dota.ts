9f6dbb26f48c0dde95dce45e4d2e718a89f2e193
unknown <hussain.qurain@outlook.com>
2023-05-23 01:02:12 +0300

    added logintosteam function

--- dota-backend/src/models/dota.ts
+++ dota-backend/src/models/dota.ts
@@ -10,11 +10,34 @@ dotenv.config();
 
 export default class Dota {
 
-    constructor() {
-    
-    }
+    private steamClient: any;
+    private steamUser: any;
+
     private steam_acc = process.env.STEAM_ACCOUNT;
     private steam_pass = process.env.STEAM_PASSWORD;
+
+    constructor() {
+        this.loginToSteam();
+    }
+    
+    async loginToSteam() {
+        this.steamClient = new Steam.SteamClient();
+        this.steamUser = new Steam.SteamUser(this.steamClient);
+        this.steamClient.on('connected', () => {
+            this.steamUser.logOn({
+                account_name: this.steam_acc,
+                password: this.steam_pass
+            });
+        });
+        this.steamClient.on('logOnResponse', (logonResp: any) => {
+            if (logonResp.eresult === Steam.EResult.OK) {
+                console.log('Logged in!');
+                this.steamUser.gamesPlayed({ games_played: [{ game_id: 570 }] });
+            } else {
+                console.log(logonResp);
+            }
+        });
+    }
 
     async getGames() {
         console.log('getGames');
