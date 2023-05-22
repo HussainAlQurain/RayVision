280d47a83a87371a67d2e998baba68a8525900f5
unknown <hussain.qurain@outlook.com>
2023-05-23 00:54:28 +0300

    removed everything

--- dota-backend/src/handlers/dota.ts
+++ dota-backend/src/handlers/dota.ts
@@ -15,26 +15,5 @@ export class DotaHandler {
             res.status(500).json(error);
         }
     }
-    //send all live matches to client
-    // async show(req: Request, res: Response) {
-    //     try{
-    //         const data = await dota.get_live_matches();
-    //         res.status(200).json({body: `${JSON.stringify(data)}`});
-    //     }
-    //     catch (error) {
-    //         res.status(500).json(error);
-    //     }
-    // }
-
-    // //get all players in a live match using one player's ID
-    // async show_players(req: Request, res: Response) {
-    //     try{
-    //         const data = await dota.get_players(parseInt(req.params.account_id));
-    //         res.status(200).json({body: `${JSON.stringify(data)}`});
-    //     }
-    //     catch (error) {
-    //         res.status(500).json(error);
-    //     }
-    // }
 
 }
